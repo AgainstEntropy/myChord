4008a55794d721520b15cc180e098ba9e44999a5
Yihao Wang <yihaowang18@fudan.edu.cn>
2023-05-02 14:45:46 +0800

    init

---
+++ MyChord/chord.py
@@ -0,0 +1,9 @@
+import os 
+import sys
+import math
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot  as plt
+import matplotlib.path    as mpath
+import matplotlib.patches as mpatches
